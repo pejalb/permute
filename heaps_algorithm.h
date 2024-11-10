d66765aade42a7bc84463d473ea158f1d09e2d14
Pedro Jorge de Albuquerque de Oliveira <pejalb@gmail.com>
2024-11-10 13:56:53 -0300

    Add a README with usage instructions.

--- heaps_algorithm.h
+++ heaps_algorithm.h
@@ -1,8 +1,11 @@
 #ifndef HEAPS_ALGORITHM_H
 #define HEAPS_ALGORITHM_H
 #include <stddef.h>
-
-typedef int permute_t;
+/* This algorithm implementation was originally intended to be used as part of a
+ * Unix filter where the assumption that the arguments could be treated as
+ * strings is valid. The typedef is merely here to facilitate adaptation of the
+ * algorithm for potential future reuse with other data types.*/
+typedef char *permute_t;
 
 void permute_recursive_heaps(permute_t *arr, size_t k, size_t size);
 #endif
